c24c669c1f3c8c3442ef4e8a10e350223acfd1c5
Arsh Sharma <sharmaarsh222@gmail.com>
2024-06-09 00:06:28 +0530

    cors update

--- src/app.js
+++ src/app.js
@@ -7,26 +7,44 @@ const app = express()
 // configurations for data using app.use()
 
 // for allowing operations/accessing on user's cookies stored in browser.
-app.use(cookieParser()) 
+app.use(cookieParser())
 
 // for cors requests. we can use cors() package or we can use "proxy".
 // app.use(cors({origin:process.env.CORS_ORIGIN, credentials:true}))
 
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true, 
-};
-app.use(cors(corsOptions))
+// const corsOptions = {
+//     origin: 'http://localhost:5173',
+//     credentials: true, 
+// };
+// app.use(cors(corsOptions))
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://blogsfordev.netlify.app'
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+app.use(cors(corsOptions));
 
 // to ensure that express understands json & to set limit for incoming json data.
-app.use(express.json({limit:"16kb"})) 
+app.use(express.json({ limit: "16kb" }))
 
 // to ensure that express understands all the data coming through url's.
-app.use(express.urlencoded({extended: true, limit:"16kb"})) 
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 
 // for storing static assets,files,images etc. in our server.
-app.use(express.static("public")) 
+app.use(express.static("public"))
 
 // user routes
 import userRoutes from './routes/user.routes.js'
